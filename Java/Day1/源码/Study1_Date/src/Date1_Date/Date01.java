b1aec7e263d45812c7e448bb74f3ed63aed7da7c
ggb <lqf159159159@gamil.com>
2025-03-24 22:12:51 +0800

    test

---
+++ Java/Day1/源码/Study1_Date/src/Date1_Date/Date01.java
@@ -0,0 +1,10 @@
+package Date1_Date;
+
+import java.util.Date;
+
+public class Date01 {
+    public static void main(String[] args) {
+        Date date1 = new Date();//获取当前系统时间
+        System.out.println(date1);
+    }
+}
